3ece011be1ae343e191aa1e518ed2eb9a0376b31
svtl <50416lglkE>
2020-02-05 21:53:57 +0100

    First Commit - from-to.online

---
+++ app/Http/Controllers/DatasizeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DatasizeController extends Controller
+{
+    public function index() {
+        return view('datasize.index');
+    }
+}
---
+++ public/0.js
@@ -0,0 +1,20 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./node_modules/ember-cli-materialize/index.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/ember-cli-materialize/index.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = {
+  name: 'ember-cli-materialize'
+};
+
+
+/***/ })
+
+}]);
