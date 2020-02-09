273cab77817f1426da2972314f6e81d13bb1709c
svtl <50416lglkE>
2020-02-09 21:14:15 +0100

    Style update for Crypto Page

--- resources/vue/components/Cryptomarketcap.vue
+++ resources/vue/components/Cryptomarketcap.vue
@@ -8,7 +8,8 @@
                     <input id="from" @keyup="getPriceForPair" v-model="from_val" type="text" class="validate">
                 </div>
                 <div class="input-field col s8 l3">
-                    <input @keyup="setCoinFrom" id="from_coin" v-model="from_coin" type="text" class="validate" placeholder="Coin Name or Symbol">
+                    <input @keyup="setCoinFrom" id="from_coin" v-model="from_coin" type="text" class="validate"
+                           placeholder="e.g.: BTC">
                     <div v-if="coinFromListFrom.length > 0" class="dropdown-search dropdown-show">
                         <a class="dropdown-search-item" v-for="c in coinFromListFrom">
                             <div @click="setFrom(c.id)" class="dropdown-item-text">{{c.name}} - {{c.symbol}}</div>
@@ -18,46 +19,49 @@
                 <div class="col s12 l2 swap-form-icon">
                     <swap-horizontal-icon :width="25" :height="25"/>
                 </div>
-                <div class="input-field col s6 l3">
-                    <input id="to" v-model="to_val" disabled type="text" class="validate">
-                </div>
-                <div class="input-field col s6 l2">
-                    <input @keyup="setCoinTo" @click="coinFromListTo = supportedCoins"  id="to_coin" v-model="to_coin" type="text" class="validate"  placeholder="Coin or Currency Symbol">
+                <div class="input-field col s12 l2">
+                    <input @keyup="setCoinTo" @click="coinFromListTo = supportedCoins" id="to_coin" v-model="to_coin"
+                           type="text" class="validate" placeholder="e.g.: usd">
                     <div v-if="coinFromListTo.length > 0" class="dropdown-search dropdown-show">
                         <a class="dropdown-search-item" v-for="c in coinFromListTo">
                             <div @click="setTo(c)" class="dropdown-item-text">{{c}}</div>
                         </a>
                     </div>
                 </div>
+                <div class="input-field col s12 l3">
+                    <input id="to" v-model="to_val" disabled type="text" class="validate">
+                </div>
             </div>
-            </div>
-
+        </div>
         <hr>
         <table class="table-coins" v-if="coins.length > 0">
             <thead>
-                <tr>
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>Market Cap</th>
-                    <th>Price</th>
-                    <th>Volume</th>
-                    <th>Circulating Supply</th>
-                </tr>
+            <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Market Cap</th>
+                <th>Price</th>
+                <th>Volume</th>
+                <th>Circulating Supply</th>
+            </tr>
             </thead>
             <tbody>
-                <tr v-for="(coin, idx) in coins">
-                    <td>{{idx + 1}}</td>
-                    <td style="display: flex">
-                        <img :src="coin.image.small" :alt="coin.name" height="20px" style="margin-right: 5px">{{coin.name}}
-                    </td>
-                    <td>{{formatNumber('$', coin.market_data.market_cap.usd)}}</td>
-                    <td>{{formatNumber('$', coin.market_data.current_price.usd)}}</td>
-                    <td>{{formatNumber('$', coin.market_data.total_volume.usd)}}</td>
-                    <!--<td>{{format({prefix: '$'})(coin.market_data.total_volume.usd))}}</td>-->
-                    <td>{{formatNumber('', coin.market_data.total_supply)}}</td>
-                </tr>
+            <tr v-for="(coin, idx) in coins">
+                <td>{{idx + 1}}</td>
+                <td style="display: flex">
+                    <img :src="coin.image.small" :alt="coin.name" height="20px" style="margin-right: 5px">{{coin.name}}
+                </td>
+                <td>{{formatNumber('$', coin.market_data.market_cap.usd)}}</td>
+                <td>{{formatNumber('$', coin.market_data.current_price.usd)}}</td>
+                <td>{{formatNumber('$', coin.market_data.total_volume.usd)}}</td>
+                <!--<td>{{format({prefix: '$'})(coin.market_data.total_volume.usd))}}</td>-->
+                <td>{{formatNumber('', coin.market_data.total_supply)}}</td>
+            </tr>
             </tbody>
         </table>
+        <div class="progress" v-else>
+            <div class="indeterminate"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -143,65 +147,65 @@
         },
 
         methods: {
-            getPing: async function() {
+            getPing: async function () {
                 let data = await CoinGeckoClient.ping();
             },
 
-            getCoins: async function() {
+            getCoins: async function () {
                 let coins = await CoinGeckoClient.coins.all();
                 this.coins = coins.data;
             },
 
-            getCoinsList: async function() {
+            getCoinsList: async function () {
                 let coins = await CoinGeckoClient.coins.list();
                 this.coinsList = coins.data;
             },
 
-            formatNumber: function(prefix, number) {
+            formatNumber: function (prefix, number) {
                 return format({prefix: prefix})(number, {noSeparator: false});
             },
 
-            setCoinFrom: function() {
+            setCoinFrom: function () {
                 if (this.from_coin.length < 2) return;
                 this.coinFromListFrom = [];
                 const _this = this;
-                this.coinsList.map(function(c) {
+                this.coinsList.map(function (c) {
                     if (c.name.indexOf(_this.from_coin) !== -1 || c.symbol.indexOf(_this.from_coin) !== -1) {
                         _this.coinFromListFrom.push(c);
                     }
                 });
             },
 
-            setCoinTo: function() {
+            setCoinTo: function () {
                 this.coinFromListTo = [];
                 const _this = this;
-                this.supportedCoins.map(function(c) {
+                this.supportedCoins.map(function (c) {
                     if (c.indexOf(_this.to_coin) !== -1) {
                         _this.coinFromListTo.push(c);
                     }
                 });
             },
 
-            setFrom: function(coin) {
+            setFrom: function (coin) {
                 this.from_coin = coin;
                 this.coinFromListFrom = [];
                 this.getPriceForPair();
             },
 
-            setTo: function(coin) {
+            setTo: function (coin) {
                 this.to_coin = coin.toLowerCase();
                 this.coinFromListTo = [];
                 this.getPriceForPair();
             },
 
-            getPriceForPair: async function() {
+            getPriceForPair: async function () {
                 if (this.from_val === "" || this.from_coin === "" || this.from_coin === null || this.to_coin === "" || this.to_coin === null) return;
                 //let params = {this.from_coin, this.to_coin};
                 let data = await CoinGeckoClient.simple.price({ids: this.from_coin, vs_currencies: this.to_coin});
                 this.to_val = this.formatNumber('', data.data[this.from_coin][this.to_coin] * this.from_val);
             },
 
-            makeMeta: function() {
+            makeMeta: function () {
                 let meta = "";
                 this.supportedCoins.map(coin => {
                     meta += "trx to " + coin + ", ";
