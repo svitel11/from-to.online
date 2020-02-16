a11cb722260f9dc8db9989445cc0f5392375389f
svtl <50416lglkE>
2020-02-16 18:34:17 +0100

    Meta Update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,7 +13,6 @@
     <meta name="description"
           content="The purpose of the application is to provide a wide range of tools that will help you to convert crypto or data storage for example, but also in the conversion and compression of images.
                     The crypto toll also has other things to offer, such as the current value of crypto coins or crypto details and tips." />
-    <meta name="description" content="With the Data Size Calculator you can calculate different units of measurement specific to the computers data. The following types can be calculated: Bit, Byte, Kilobit, Kilobyte, Megabit, Megabyte, Gigabit, Gigabyte, Terabit, Terabyte, Petabit, Petabyte" />
 @endsection
 @section('content')
     @include('home.content')
