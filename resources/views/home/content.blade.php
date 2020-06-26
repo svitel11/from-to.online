049bd32fa0f47eb94d146fcc46061cbc37e7a34e
greatmission <eam.greatmission@gmail.com>
2020-06-27 01:35:27 +0200

    Minor changes. Add README.md

--- resources/views/home/content.blade.php
+++ resources/views/home/content.blade.php
@@ -1,14 +1,14 @@
 <div id="app"></div>
 <div id="home">
     <div class="home-item">
-        <h3><a href="{{route('crypto')}}"><b>Crypto</b> - Converter & Market Cap</a></h3>
+        <h1><a href="{{route('crypto')}}"><b>Crypto</b> - Converter & Market Cap</a></h1>
         <p>The converter calculates in real time the value of crypto in other crypto or fiat.<br>
             As example <b>Bitcoin to USD</b> or <b>Bitcoin to EUR</b> or <b>Bitcoin to Ethereum</b> and much more.
             In total there are about 7000 cryptos available whose value you can calculate in the converter.</p>
         <p>Also here you can see the real-time prices of the most important crypto currencies.</p>
     </div><hr>
     <div class="home-item">
-        <h3><a href="{{route('image')}}"><b>Image</b> - Converter & Compressor</a></h3>
+        <h1><a href="{{route('image')}}"><b>Image</b> - Converter & Compressor</a></h1>
         <p>
             The Image Converter can help you to change the extension of your image. As example you can convert<br>
             - jpg to png<br>
@@ -19,13 +19,13 @@
         </p>
     </div><hr>
     <div class="home-item">
-        <h3><a href="{{route('datastorage')}}"><b>Data Storage</b> - Converter</a></h3>
+        <h1><a href="{{route('datastorage')}}"><b>Data Storage</b> - Converter</a></h1>
         <p>
             Use this data storage conversion tool to convert instantly between: <i>Bit, Byte, Kilobit, Kilobyte, Megabit, Megabyte, Gigabit, Gigabyte, Terabit, Terabyte, Petabit and Petabyte</i>.
         </p>
     </div><hr>
     <div class="home-item">
-        <h3><a href="{{route('mass')}}"><b>Mass/Weight</b> - Converter</a></h3>
+        <h1><a href="{{route('mass')}}"><b>Mass/Weight</b> - Converter</a></h1>
         <p>
             The following units of measurement of mass and weight are available in the converter: <i>Microgram (mcg), Milligram (mg), Gram (g), Kilogram (kg), Metric Tonne (mt), Ounce (oz), Pound (lb) and Ton (t)</i>.
         </p>
